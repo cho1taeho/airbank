e36c456e9eed8325d9c9c706dd883e82e1d3c995
cho1taeho <cow12dog@gmail.com>
2023-09-20 10:14:35 +0900

    0920

--- app/src/main/java/com/example/myapplication/screens/ChildSavingsApplication.kt
+++ app/src/main/java/com/example/myapplication/screens/ChildSavingsApplication.kt
@@ -51,14 +51,18 @@ import android.content.Intent
 import android.icu.util.Calendar
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
@@ -72,11 +76,14 @@ import androidx.navigation.compose.rememberNavController
 //@Preview
 @Composable
 fun ChildSavingsApplication(navController: NavController) {
-    var textFieldValue by remember { mutableStateOf("") }
+    var targetValue by remember { mutableStateOf(TextFieldValue()) }
+    var priceValue by remember { mutableStateOf(TextFieldValue()) }
+    var requestPriceValue by remember { mutableStateOf(TextFieldValue()) }
 
     val calendar: Calendar = Calendar.getInstance()
     var today by remember { mutableStateOf("") }
-    today = "${calendar.get(Calendar.YEAR)}-${calendar.get(Calendar.MONTH) + 1}-${calendar.get(Calendar.DAY_OF_MONTH)}"
+    today =
+        "${calendar.get(Calendar.YEAR)}-${calendar.get(Calendar.MONTH) + 1}-${calendar.get(Calendar.DAY_OF_MONTH)}"
 
     var showDatePicker by remember { mutableStateOf(false) }
     var selectedDate by remember { mutableStateOf("") }
@@ -89,10 +96,12 @@ fun ChildSavingsApplication(navController: NavController) {
 
 
     if (showDatePicker) {
-        val datePickerDialog = DatePickerDialog(context, { _, selectedYear, selectedMonth, selectedDay ->
-
-        }, year, month, day)
+        val datePickerDialog =
+            DatePickerDialog(context, { _, selectedYear, selectedMonth, selectedDay ->
+                showDatePicker = false
+            }, year, month, day)
         datePickerDialog.show()
+        showDatePicker = false
     }
 
     Column(
@@ -134,30 +143,40 @@ fun ChildSavingsApplication(navController: NavController) {
         Spacer(modifier = Modifier.height(20.dp))
 
         Text("목표")
-
+        val targetMaxLength = 10
         TextField(
-            value = textFieldValue,
+            value = targetValue,
             onValueChange = { newValue ->
-                textFieldValue = newValue
+                if (newValue.text.length <= targetMaxLength) {
+                    targetValue = newValue
+                }
             },
 //            label = { Text("목표를 입력하세요.") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color(0xFFD6F2FF)),
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Text
+            ),
+
+
             )
-        )
 
         Spacer(modifier = Modifier.height(20.dp))
 
         Text("가격")
 
         TextField(
-            value = textFieldValue,
+            value = priceValue,
             onValueChange = { newValue ->
-                textFieldValue = newValue
+                priceValue = newValue
             },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color(0xFFD6F2FF)),
 //            label = { Text("가격을 입력하세요.") },
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text
+                keyboardType = KeyboardType.Number
             )
         )
 
@@ -166,50 +185,37 @@ fun ChildSavingsApplication(navController: NavController) {
         Text("요청 금액")
 
         TextField(
-            value = textFieldValue,
+            value = requestPriceValue,
             onValueChange = { newValue ->
-                textFieldValue = newValue
+                requestPriceValue = newValue
             },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color(0xFFD6F2FF)),
 //            label = { Text("요청 가격을 입력하세요.") },
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text
+                keyboardType = KeyboardType.Number
             )
         )
 
         Spacer(modifier = Modifier.height(20.dp))
 
-//        Text("기간")
-//
-//        Text(selectedDate)  // 선택한 날짜를 표시
-//        Button(onClick = { showDatePicker = true }) {  // 버튼 클릭시 날짜 선택기 보여주기
-//            Text("날짜 선택")
-//        }
+        val monthNumbers = (3..12).toList()
+        var selectedMonths by remember { mutableStateOf(3) }
+        var expanded by remember { mutableStateOf(false) }
+
         Text("기간: $today ~ $selectedDate")
 
         Button(onClick = { showDatePicker = true }) {
             Text("날짜 선택")
         }
 
-        if (showDatePicker) {
-            val context = LocalContext.current
-            val datePickerDialog = DatePickerDialog(
-                context,
-                { _, year, month, dayOfMonth ->
-                    selectedDate = "$year-${month + 1}-$dayOfMonth"
-                    showDatePicker = false
-                },
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH)
-            )
-            datePickerDialog.setOnCancelListener {
-                showDatePicker = false
-            }
-            datePickerDialog.show()
-        }
-    }
 
         Spacer(modifier = Modifier.height(20.dp))
 
         Text("첨부하기")
     }
+}
+
+
+
