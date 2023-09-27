e85ba96506fad1a0e825f680e031642eb9b44210
Chae Huichan <eqholic1125@gmail.com>
2023-09-27 13:35:31 +0900

    feat: 땡겨쓰기 스크린 구현

--- app/src/main/java/com/example/myapplication/screens/LoanScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/LoanScreen.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import android.content.Intent
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
@@ -58,5 +59,118 @@ import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun LoanScreen(navController: NavController) {
-    Text("HD")
+    Column(
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(145.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(color = Color(0xffD6F2FF))
+                    .padding(horizontal = 16.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(start = 13.dp)
+                ) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text(
+                        "땡겨쓰기 가능 금액",
+                        fontSize = 16.sp,
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(
+                        "2,500,000원",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text(
+                        "현도 금액: ",
+                        fontSize = 14.sp,
+                        color = Color(0xff515151)
+                    )
+                    Spacer(modifier = Modifier.size(5.dp))
+                    Text(
+                        "사용 금액: ",
+                        fontSize = 14.sp,
+                        color = Color(0xff515151)
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.size(17.dp))
+        Divider(
+            color = Color(0xffCBCBCB),
+            thickness = 1.dp
+        )
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(145.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(color = Color.LightGray)
+                    .padding(horizontal = 16.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(start = 13.dp)
+                ) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text(
+                        "이자",
+                        fontSize = 16.sp,
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(
+                        "150,000원",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text(
+                        "이번 달 이자: ",
+                        fontSize = 14.sp,
+                        color = Color(0xff515151)
+                    )
+                    Spacer(modifier = Modifier.size(5.dp))
+                    Text(
+                        "연체 이자: ",
+                        fontSize = 14.sp,
+                        color = Color(0xff515151)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.size(17.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(color = Color.LightGray)
+                    .padding(horizontal = 16.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(start = 13.dp)
+                ) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text(
+                        "신용점수",
+                        fontSize = 16.sp,
+                    )
+                    ScoreBar(score = 500)
+                    CreditPoint()
+                }
+            }
+        }
+    }
 }
