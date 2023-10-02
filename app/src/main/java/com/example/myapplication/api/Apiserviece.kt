d81de32f7da7880cd39439ee78c09a43b553fcc9
cho1taeho <cow12dog@gmail.com>
2023-10-02 17:19:09 +0900

    loanapi

--- app/src/main/java/com/example/myapplication/api/Apiserviece.kt
+++ app/src/main/java/com/example/myapplication/api/Apiserviece.kt
@@ -1,15 +1,36 @@
 package com.example.myapplication.api
 
+import com.example.myapplication.model.AccountCheckResponse
+import com.example.myapplication.model.AccountHistoryCheckResponse
+import com.example.myapplication.model.AccountRegisterRequest
+import com.example.myapplication.model.AccountRegisterResponse
+import com.example.myapplication.model.BonusRequest
+import com.example.myapplication.model.BonusResponse
 import com.example.myapplication.model.BonusSavingsRequest
 import com.example.myapplication.model.BonusSavingsResponse
 import com.example.myapplication.model.CancelSavingsRequest
 import com.example.myapplication.model.CancelSavingsResponse
+import com.example.myapplication.model.ConfiscationResponse
+import com.example.myapplication.model.ConfiscationTransferRequest
+import com.example.myapplication.model.ConfiscationTransferResponse
 import com.example.myapplication.model.CreateSavingsItemRequest
 import com.example.myapplication.model.CreateSavingsItemResponse
+import com.example.myapplication.model.InterestRepaymentRequest
+import com.example.myapplication.model.InterestRepaymentResponse
+import com.example.myapplication.model.InterestResponse
+import com.example.myapplication.model.LoanChargeRequest
+import com.example.myapplication.model.LoanChargeResponse
+import com.example.myapplication.model.LoanRepaymentRequest
+import com.example.myapplication.model.LoanRepaymentResponse
 import com.example.myapplication.model.LoanResponse
+import com.example.myapplication.model.LoanStartRequest
+import com.example.myapplication.model.LoanStartResponse
 import com.example.myapplication.model.SavingsRemitRequest
 import com.example.myapplication.model.SavingsRemitResponse
 import com.example.myapplication.model.SavingsResponse
+import com.example.myapplication.model.TaxResponse
+import com.example.myapplication.model.TaxTransferRequest
+import com.example.myapplication.model.TaxTransferResponse
 import com.example.myapplication.model.UpdateSavingsRequest
 import com.example.myapplication.model.UpdateSavingsResponse
 import retrofit2.Response
@@ -41,9 +62,49 @@ interface ApiService {
     suspend fun getLoan(@Query("group_id") groupId: Int): Response<LoanResponse>
 
     @POST("/accounts")
-    suspend fun accountRegister()
+    suspend fun accountRegister(@Body request: AccountRegisterRequest) : Response<AccountRegisterResponse>
+
+    //계좌 내역 조회
+    @GET("/accounts/history")
+    suspend fun accountHistory(@Query("accountType") accountType: String): Response<AccountHistoryCheckResponse>
+
+    //계좌 잔액 조회
+    @GET("/accounts")
+    suspend fun accountCheck(): Response<AccountCheckResponse>
+
+    @GET("/funds/tax")
+    suspend fun taxCheck(@Query("group_id") groupId: Int): Response<TaxResponse>
+
+    @POST("/funds/tax")
+    suspend fun taxTransfer(@Body request: TaxTransferRequest) : Response<TaxTransferResponse>
+
+    @POST("/funds/bonus")
+    suspend fun bonusTransfer(@Query("group_id") groupId: Int,@Body request: BonusRequest): Response<BonusResponse>
 
 
+    @GET("/funds/interest")
+    suspend fun interestCheck(@Query("group_id") groupId: Int): Response<InterestResponse>
+
+    @POST("/funds/interest")
+    suspend fun interestRepayment(@Body request: InterestRepaymentRequest): Response<InterestRepaymentResponse>
+
+    @GET("/funds/confiscation")
+    suspend fun confiscationCheck(@Query("group_id") groupId: Int): Response<ConfiscationResponse>
+
+    @POST("/funds/confiscation")
+    suspend fun confiscationTransfer(@Body request: ConfiscationTransferRequest): Response<ConfiscationTransferResponse>
+
+
+
+    @POST("/loans")
+    suspend fun loanStart(@Body request: LoanStartRequest): Response<LoanStartResponse>
+
+
+    @POST("/loans/repaid")
+    suspend fun loanRepayment(@Body request: LoanRepaymentRequest): Response<LoanRepaymentResponse>
+
+    @POST("/loans/charge")
+    suspend fun loanCharge(@Query("group_id") groupId: Int, @Body request: LoanChargeRequest): Response<LoanChargeResponse>
 
 
 
--- app/src/main/java/com/example/myapplication/model/LoanResponse.kt
+++ app/src/main/java/com/example/myapplication/model/LoanResponse.kt
@@ -11,3 +11,54 @@ data class LoanResponse (
         val loanLimit: Long
     )
 }
+
+
+data class LoanStartRequest(
+    val amount: Int
+)
+
+data class LoanStartResponse(
+    val code: String,
+    val message: String,
+    val data: Data
+){
+    data class Data(
+        val amount: Int
+    )
+}
+
+data class LoanRepaymentRequest(
+    val amount: Int
+)
+
+data class LoanRepaymentResponse(
+    val code: String,
+    val message: String,
+    val data: Data
+){
+    data class Data(
+        val amount: Int,
+        val loanBalance: Int
+    )
+}
+
+
+data class LoanChargeRequest(
+    val amount: Int
+)
+
+data class LoanChargeResponse(
+    val code: String,
+    val message: String,
+    val data: Data
+){
+    data class Data(
+        val id: Int,
+        val amount: Int,
+        val apiCreateAt : String,
+        val transactionType : String,
+        val transactionDistinction : String,
+        val transactionPartner : String
+    )
+}
+
