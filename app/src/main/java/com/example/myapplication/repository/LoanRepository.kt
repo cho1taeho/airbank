295fbe1dd34d8edf624873cc734dc8e1a6080b46
Chae Huichan <eqholic1125@gmail.com>
2023-10-02 17:17:12 +0900

    refactor: 땡겨쓰기 API 연결

--- app/src/main/java/com/example/myapplication/repository/LoanRepository.kt
+++ app/src/main/java/com/example/myapplication/repository/LoanRepository.kt
@@ -1,10 +1,21 @@
 package com.example.myapplication.repository
 
 import com.example.myapplication.api.ApiService
+import com.example.myapplication.model.LoanResponse
+import com.example.myapplication.model.Resource
+import com.example.myapplication.model.State
+import retrofit2.Response
 import javax.inject.Inject
 
 class LoanRepository @Inject constructor(
     private val apiService: ApiService
 ){
-    suspend fun getLoan(groupId: Int) = apiService.getLoan(groupId)
+    suspend fun getLoan(groupId: Int): Resource<LoanResponse> {
+        val response = apiService.getLoan(groupId)
+        return if (response.isSuccessful) {
+            Resource(State.SUCCESS, response.body(), "SUCCESS")
+        } else {
+            Resource(State.ERROR, null, "ERROR")
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/viewmodel/LoanViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/LoanViewModel.kt
@@ -1,44 +1,42 @@
 package com.example.myapplication.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myapplication.model.LoanResponse
+import com.example.myapplication.model.Resource
+import com.example.myapplication.model.State
 import com.example.myapplication.repository.LoanRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoanViewModel @Inject constructor(
     private val loanRepository: LoanRepository
-): ViewModel() {
+) : ViewModel() {
 
-    val loanData = MutableLiveData<LoanResponse.Data>()
+    private val _loanState =
+        MutableStateFlow<Resource<LoanResponse>>(Resource(State.LOADING, null, null))
 
-    private val _error = MutableLiveData<String>()
-    val error: LiveData<String> get() = _error
-
-    private val _navigateToLoan = MutableLiveData<Boolean>()
-    val navigateToLoan: LiveData<Boolean> get() = _navigateToLoan
-
+    val loanState: StateFlow<Resource<LoanResponse>> get() = _loanState
     init {
         getLoan()
     }
 
-    private fun getLoan() {
-        viewModelScope.launch {
-            try {
-                val response = loanRepository.getLoan(1)
-                if (response.isSuccessful) {
-                    loanData.postValue(response.body()?.data)
-                } else {
-                    _error.value = "Server Response Error: ${response.code()}"
-                }
-            } catch (e: Exception) {
-                _error.value = "Network Error: ${e.localizedMessage}"
-            }
+    fun getLoan() = viewModelScope.launch {
+        _loanState.emit(Resource(State.LOADING, null, null))
+        try {
+            val response = loanRepository.getLoan(1)
+            _loanState.emit(response)
+            Log.d("DEBUG", "_loanState is set: ${_loanState.value}")
+        } catch (e: Exception) {
+            _loanState.emit(Resource(State.ERROR, null, e.localizedMessage))
         }
+
     }
 }
