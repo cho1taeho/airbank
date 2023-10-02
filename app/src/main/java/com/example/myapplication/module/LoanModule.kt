3776d94586aac0fc1835c42db20747b4d5f101bc
cho1taeho <cow12dog@gmail.com>
2023-10-02 20:39:18 +0900

    1002

--- app/src/main/java/com/example/myapplication/module/LoanModule.kt
+++ app/src/main/java/com/example/myapplication/module/LoanModule.kt
@@ -1,22 +1,25 @@
-//package com.example.myapplication.module
-//
-//import com.example.myapplication.api.ApiService
-//import com.example.myapplication.network.RetrofitBuilder
-//import com.example.myapplication.repository.LoanRepository
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.components.SingletonComponent
-//import javax.inject.Singleton
-//
-//@Module
-//@InstallIn(SingletonComponent::class)
-//class LoanModule {
-//    @Provides
-//    @Singleton
-//    fun provideApiService(): ApiService = RetrofitBuilder.createApiService()
-//
-//    @Provides
-//    @Singleton
-//    fun provideLoanRepository(apiService: ApiService): LoanRepository = LoanRepository(apiService)
-//}
+package com.example.myapplication.module
+
+import com.example.myapplication.api.ApiService
+import com.example.myapplication.network.RetrofitBuilder
+import com.example.myapplication.repository.LoanRepository
+import com.example.myapplication.repository.SavingsRepository
+import com.example.myapplication.repository.AccountRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LoanModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService = RetrofitBuilder.createApiService()
+
+    @Provides
+    @Singleton
+    fun provideLoanRepository(apiService: ApiService): LoanRepository = LoanRepository(apiService)
+}
