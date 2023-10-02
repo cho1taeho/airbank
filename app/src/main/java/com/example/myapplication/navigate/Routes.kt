f7127155e99018c730817cd351d36003302e34bd
HidenLee <hyundo1995@gmail.com>
2023-10-02 21:58:19 +0900

    route role구분

--- app/src/main/java/com/example/myapplication/navigate/Routes.kt
+++ app/src/main/java/com/example/myapplication/navigate/Routes.kt
@@ -1,14 +1,21 @@
 package com.example.myapplication.navigate
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.myapplication.AirbankApplication
 import com.example.myapplication.screens.AuthScreen
 import com.example.myapplication.screens.BottomNavItem
+import com.example.myapplication.screens.ChildMainScreen
 import com.example.myapplication.screens.ChildSavingsApplication
 import com.example.myapplication.screens.ChildSavingsScreen
 import com.example.myapplication.screens.ChildSavingsTransferScreen
@@ -28,9 +35,17 @@ import com.example.myapplication.viewmodel.SavingsViewModel
 @RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun AppNavigation(navController: NavHostController){
+    var userRole by remember { mutableStateOf("") }
+    userRole = AirbankApplication.prefs.getString("role","")
     NavHost(navController = navController, startDestination = "First") {
         composable(BottomNavItem.Main.screenRoute) {
-            MainScreen(navController = navController)
+            Log.d("userRole",userRole)
+            if (userRole == "CHILD"){
+                ChildMainScreen(navController = navController)
+            }
+            else{
+                MainScreen(navController = navController)
+            }
         }
         composable(BottomNavItem.Savings.screenRoute) {
             SavingsScreen(navController = navController)
