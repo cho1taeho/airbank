22d40de7fd2580c45f55e7e14d6a8fb4fd130035
HyundoLee <hyundo1995@gmail.com>
2023-10-04 11:02:10 +0900

    급히 수정한 ChildMainScreen

--- app/src/main/java/com/example/myapplication/screens/ChildMainScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/ChildMainScreen.kt
@@ -1,10 +1,9 @@
 package com.example.myapplication.screens
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,39 +14,34 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.myapplication.AirbankApplication
 import com.example.myapplication.R
+import com.example.myapplication.model.GETGroupsResponse
+import com.example.myapplication.network.HDRetrofitBuilder
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 @Composable
 fun ChildMainScreen(navController: NavController) {
+    val viewModel : ChildMainViewModel = viewModel()
 
     val name = AirbankApplication.prefs.getString("name","")
     var imagepath = AirbankApplication.prefs.getString("imageUrl","")
@@ -211,5 +205,30 @@ fun Quiz(){
 //            .clickable()
     ){
         Text("퀴즈")
+    }
+}
+
+class ChildMainViewModel @Inject constructor() : ViewModel() {
+
+    var childs : List<GETGroupsResponse.Data> = emptyList()
+    init {
+        getGroup()
+    }
+    private fun getGroup() {
+        val tag = "getGroup"
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = HDRetrofitBuilder.HDapiService().getGroups()
+                val getGroupsResponse = response.body()
+                if (getGroupsResponse != null) {
+                    Log.d(tag, getGroupsResponse.toString())
+                    childs = getGroupsResponse.data
+                    AirbankApplication.prefs.setString("group_id",childs.first().groupId.toString())
+                    Log.d(tag,"group_id = "+AirbankApplication.prefs.getString("group_id",""))
+                }
+            } catch (e: Exception) {
+                Log.e(tag, e.toString())
+            }
+        }
     }
 }
