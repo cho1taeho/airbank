c598ca1680c2b24a2bc52d237541eccb2d8061df
HyundoLee <hyundo1995@gmail.com>
2023-10-05 12:53:50 +0900

    ChildMainScreen ChildCard 변경

--- app/src/main/java/com/example/myapplication/screens/ChildMainScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/ChildMainScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,12 +15,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -34,8 +38,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.example.myapplication.AirbankApplication
 import com.example.myapplication.R
+import com.example.myapplication.model.GETGroupsResponse
 import com.example.myapplication.network.HDRetrofitBuilder
 import com.kakao.sdk.user.UserApiClient
 import kotlinx.coroutines.Dispatchers
@@ -66,11 +72,57 @@ fun ChildMainScreen(navController: NavController) {
         imagepath = "local"
     }
     Column {
-//        ChildCard(mainImage = imagepath, mainName = name)
+        ChildCard(viewModel)
         ChildBody(navController = navController)
         Spacer(modifier = Modifier.size(16.dp))
         Quiz()
     }
+}
+
+@Composable
+fun ChildCard(viewModel: ChildMainViewModel) {
+    var selectChild by remember { mutableStateOf(GETGroupsResponse.Data.Member(0,0,"","",0)) }
+    selectChild = viewModel.child ?: viewModel.childs.firstOrNull() ?: GETGroupsResponse.Data.Member(0,0,"","",0)
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color(0xFF5FCFEF))
+    ){
+        Column (
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if(selectChild?.imageUrl.isNullOrEmpty()){
+                    Image(
+                        painter = painterResource(R.drawable.karina),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(CircleShape)
+                    )
+                }else{
+                    AsyncImage(
+                        model = selectChild?.imageUrl ?: "",
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(CircleShape)
+                    )
+                }
+                Spacer(Modifier.width(16.dp))
+                Text("자녀 ${selectChild?.name ?: ""} 님의\n지갑을 관리하고 있습니다.")
+            }
+            Text("신용점수")
+//            val creditPoint = AirbankApplication.prefs.getString("creditScore", "")
+            val creditPoint = selectChild?.creditScore ?: 0
+            ScoreBar(creditPoint)
+        }
+    }
+    Spacer(modifier = Modifier.size(20.dp))
 }
 
 @Composable
@@ -229,8 +281,24 @@ fun Quiz(){
 
 class ChildMainViewModel @Inject constructor() : ViewModel() {
 
-    var groupid = 0
+    var groupid by mutableIntStateOf(0)
+    var childs : List<GETGroupsResponse.Data.Member> = emptyList()
+    var child: GETGroupsResponse.Data.Member? by mutableStateOf(null)
 
+//    fun getGroup() {
+//        val tag = "getGroup"
+//        viewModelScope.launch(Dispatchers.IO) {
+//            try {
+//                val response = HDRetrofitBuilder.HDapiService().getGroups()
+//                if (response.body() != null) {
+//                    val getGroupsResponse = response.body()!!.data
+//                    groupid = getGroupsResponse.members.first().groupId
+//                    AirbankApplication.prefs.setString("group_id",groupid.toString())
+//                    Log.d(tag,groupid.toString())
+//                } else { Log.e("MainViewModel", "Response not successful: ${response.code()}") }
+//            } catch (e: Exception) { Log.e("MainViewModel", "Error: ${e.message}")  }
+//        }
+//    }
     fun getGroup() {
         val tag = "getGroup"
         viewModelScope.launch(Dispatchers.IO) {
@@ -238,11 +306,15 @@ class ChildMainViewModel @Inject constructor() : ViewModel() {
                 val response = HDRetrofitBuilder.HDapiService().getGroups()
                 if (response.body() != null) {
                     val getGroupsResponse = response.body()!!.data
-                    groupid = getGroupsResponse.members.first().groupId
-                    AirbankApplication.prefs.setString("group_id",groupid.toString())
-                    Log.d(tag,groupid.toString())
-                } else { Log.e("MainViewModel", "Response not successful: ${response.code()}") }
-            } catch (e: Exception) { Log.e("MainViewModel", "Error: ${e.message}")  }
+                    childs = getGroupsResponse.members
+                    if (childs.isNullOrEmpty()){Log.e("ChildMainViewModel","Child empty")}
+                    else{
+                        AirbankApplication.prefs.setString("group_id",childs.first().groupId.toString())
+                        child = childs.first()
+                        Log.d(tag,childs.first().groupId.toString())
+                    }
+                } else { Log.e("ChildMainViewModel", "Response not successful: ${response.code()}") }
+            } catch (e: Exception) { Log.e("ChildMainViewModel", "Error: ${e.message}")  }
         }
     }
 }
